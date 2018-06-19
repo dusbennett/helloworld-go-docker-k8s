802d092fd60111447b5d907c541d46369826fe3e
Dustin Bennett <dubennett@alt01115.csnzoo.com>
2018-06-19 16:20:53 -0400

    adding the default dockerfile, updating readme and print for IPs. this is now working through docker on mac exposed to localhost

--- README.md
+++ README.md
@@ -43,3 +43,17 @@ Run env command
 ```
 eval $(docker-machine env default)
 ```
+
+A bit of trickiness - you have to open the virtualbox config, navigate to "network" and add a forwarding rule for your default docker image.
+
+Build the docker image:
+
+```
+docker build -t helloworld .
+```
+
+Run the thing with port exposed (this really should be dynamic not hard at 3000):
+
+```
+docker run -p 3000:3000 helloworld
+```
--- helloworld.go
+++ helloworld.go
@@ -3,13 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net"
+	"os"
 )
 
 func main() {
   fmt.Println("Hello, starting app")
+  printIPs()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":3000", nil)
+}
+
+
+func printIPs() {
+		addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+			}
+		}
+	}
 }
